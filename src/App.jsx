b99e9ab19041f30861f3f06fdd793ea9d41caa2f
Annikahlaursen <Annikahlaursen@gmail.com>
2025-09-03 09:49:34 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import NavBar from "./components/NavLink";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div className="page">
+      <h1>Projects Page</h1>
+      <p>This is the Projects page.</p>
+    </div>
+  );
+}
